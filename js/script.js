0f710c6c929cad2c1187d03f9c6707e63bdbc7bb
Konstanty Silwanowicz <kosto4ka1035@gmail.com>
2025-03-09 11:34:19 +0100

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
 'use strict';
 
-function titleClickHandler(){
+function titleClickHandler(event) {
   console.log('Link was clicked!');
+
+  // Prevent the default action of the link
+  event.preventDefault();
+
+  /* remove class 'active' from all article links  */
+  const links = document.querySelectorAll('.titles a');
+  for (let link of links) {
+    link.classList.remove('active');
+  }
+
+  /* add class 'active' to the clicked link */
+  event.currentTarget.classList.add('active');
+
+  /* remove class 'active' from all articles */
+  const articles = document.querySelectorAll('.post');
+  for (let article of articles) {
+    article.classList.remove('active');
+  }
+
+  /* get 'href' attribute from the clicked link */
+  const href = event.currentTarget.getAttribute('href');
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(href);
+
+  /* add class 'active' to the correct article */
+  if (targetArticle) {
+    targetArticle.classList.add('active');
+  }
 }
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
