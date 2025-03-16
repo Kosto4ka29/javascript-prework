d739211c3b3ac06a48a4087d20dae243ea07c719
Kosto4ka29 <135865@student.uni.opole.pl>
2025-03-16 09:36:58 +0100

    initial commit\=

--- js/script.js
+++ js/script.js
@@ -8,3 +8,52 @@ function titleClickHandler(){
 const buttonTest = document.getElementById('button-test');
 
 buttonTest.addEventListener('click', titleClickHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generateTags() {
+  /* find all articles */
+  const articles = document.querySelectorAll(".post");
+
+  /* START LOOP: for every article */
+  articles.forEach(article => {
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector(".post-tags");
+
+    /* make html variable with empty string */
+    let html = "";
+
+    /* get tags from data-tags attribute */
+    const tags = article.getAttribute("data-tags");
+
+    /* split tags into array */
+    const tagsArray = tags.split(" ");
+
+    /* START LOOP: for each tag */
+    tagsArray.forEach(tag => {
+      /* generate HTML of the link */
+      const tagHTML = `<a href="#tag-${tag}" class="tag-link">${tag}</a> `;
+
+      /* add generated code to html variable */
+      html += tagHTML;
+    });
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
+  });
+  /* END LOOP: for every article */
+}
+
+generateTags();
